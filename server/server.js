551d3fb77970a903f168bfd181d2a596af93ecf5
pavan <mendepavan2004@gmail.com>
2025-03-03 23:14:56 +0530

    Fix line endings in .gitignore

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app=express()
 
 const port= process.env.PORT || 4000
 connectDB()
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['http://localhost:5174']
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin:allowedOrigins,credentials:true}))
